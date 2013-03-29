347aed26be6dcdcae699b42d52fe79c5e0dde8c8
Wontae Yang <wontaeyang@gmail.com>
2013-03-28 20:53:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsApp::Application.config.secret_token = '214136dfed64ca545f37bdea3eabba0b3d2d1980a3c8eb814f71159b24b5560b940e9d5231536d255e72ca84fb9dfee25c7a61d49e0f357dcc9effb1f9745820'
