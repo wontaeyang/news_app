404b6ab8bd8151e90b46c932b97bc1d8b785586f
Wontae Yang <wontaeyang@gmail.com>
2013-04-07 23:13:25 -0400

    Permission Change For Comments

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
 	
-	before_filter :authenticate_user!, except: [:index]
+	before_filter :authenticate_user!, except: [:index, :show]
 	
 	def new
 		@link = Link.new
